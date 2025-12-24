88760b65a5a2c44d8b24422b3b8cb2d240b9b535
Ramchandra Tharu <ramchandrachaudhary2003@gmail.com>
2025-12-24 13:27:03 +0530

    models crearted

---
+++ ai-tool/models/User.ts
@@ -0,0 +1,27 @@
+import mongoose, { schema, model, models } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser {
+    email: string;
+    password: string;
+    _id?: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const UserSchema = new schema<IUser>({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    {
+        timestamps: true,
+    },
+    
+
+);
